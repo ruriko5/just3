b1ec3c7786e96e3ac799bed4473bb0b1f0b1403d
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-10-13 02:24:28 +0900

    feat: logout button

---
+++ components/logout-button.tsx
@@ -0,0 +1,15 @@
+import { LogOutIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { signOut } from "@/auth/actions";
+import { cn } from "@/lib/utils";
+
+export const LogoutButton = ({ className }: React.ComponentProps<"form">) => {
+  return (
+    <form action={signOut} className={cn(className)}>
+      <Button variant="outline" className="w-full">
+        <LogOutIcon />
+        <span>Log out</span>
+      </Button>
+    </form>
+  );
+};
