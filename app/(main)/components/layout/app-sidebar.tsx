92b3101d906463ba1836e5e1a8d64a23fb22be41
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-10-13 21:12:15 +0900

    feat: minimal layout

---
+++ app/(main)/components/layout/app-sidebar.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+
+export const AppSidebar = ({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) => {
+  return (
+    <Sidebar
+      className="top-(--header-height) h-[calc(100svh-var(--header-height))]!"
+      {...props}
+      collapsible="icon"
+    >
+      <SidebarContent>
+        {/* <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
+      </SidebarContent>
+      {/* <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter> */}
+    </Sidebar>
+  );
+};
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,30 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Header } from "@/app/(main)/components/layout/header";
+import { AppSidebar } from "./components/layout/app-sidebar";
+
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div className="[--header-height:calc(--spacing(14))]">
+      <SidebarProvider className="flex flex-col">
+        <Header />
+        <div className="flex flex-1">
+          <AppSidebar />
+          <SidebarInset>
+            <div className="flex flex-1 flex-col gap-4 p-4">
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+              </div>
+              <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+            </div>
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </div>
+  );
 }
