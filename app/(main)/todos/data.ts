02764f47b182f965cae578b302dcf606f38e872f
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-10-17 13:11:42 +0900

    feat: getTodos, getTodo, isThreeOrMore

--- app/(main)/todos/data.ts
+++ app/(main)/todos/data.ts
@@ -1,4 +1,43 @@
 import "server-only";
 
-export const getTodos = () => {};
-export const getTodo = () => {};
+import { currentUser } from "@/app/auth/data";
+import { createClient } from "@/supabase/server";
+import { cache } from "react";
+
+export const getTodos = cache(async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("Unauthorized");
+
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("todos")
+    .select()
+    .order("todoCreatedAt", { ascending: false });
+
+  if (error) throw new Error("Get Todos Error");
+
+  return data;
+});
+
+export const getTodo = cache(async (id: number) => {
+  const user = await currentUser();
+  if (!user) throw new Error("Unauthorized");
+
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("todos")
+    .select()
+    .eq("id", id)
+    .single();
+
+  if (error) throw new Error("Get Todo Error");
+
+  return data;
+});
+
+export const isThreeOrMore = cache(async () => {
+  const todos = await getTodos();
+
+  if (todos.length >= 3) return true;
+  return false;
+});
