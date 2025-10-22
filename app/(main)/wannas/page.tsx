61ae747038e934e59fb85edf3edbf4609ee56b30
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-10-22 23:57:43 +0900

    feat: wannas page

--- app/(main)/wannas/page.tsx
+++ app/(main)/wannas/page.tsx
@@ -1,3 +1,18 @@
-export default function WannasPage() {
-  return <main>Wannas Page</main>;
+import { verifyUser } from "@/app/auth/data";
+import { getWannas } from "./data";
+import { TaskAddForm } from "../components/task/task-add-form";
+import { TaskList } from "../components/task/task-list";
+
+export default async function WannasPage() {
+  await verifyUser();
+
+  const wannas = await getWannas();
+  return (
+    <main className="flex flex-1 flex-col gap-4 max-w-2xl mx-auto container">
+      <h2>Wannas Page</h2>
+
+      <TaskAddForm />
+      <TaskList tasks={wannas} className="flex flex-col gap-4" />
+    </main>
+  );
 }
