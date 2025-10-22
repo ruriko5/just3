5bf3595a0b731b266fcce79608baad02a2663332
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-10-23 00:26:40 +0900

    feat: wannas/actions

--- app/(main)/wannas/actions.ts
+++ app/(main)/wannas/actions.ts
@@ -1,7 +1,64 @@
 "use server";
 
-export const createWanna = () => {};
-export const updateWanna = () => {};
-export const deleteWanna = () => {};
+import { currentUser } from "@/app/auth/data";
+import { TaskFormData, taskFormSchema } from "@/schemas/task-form-schema";
+import { createClient } from "@/supabase/server";
+import { getWanna } from "./data";
 
-export const migrateToTodo = () => {};
+export const createWanna = async (data: TaskFormData) => {
+  const user = await currentUser();
+  if (!user) throw new Error("Unauthorized");
+
+  const res = taskFormSchema.parse(data);
+
+  const supabase = await createClient();
+  const { error } = await supabase.from("wannas").insert({
+    ...res,
+  });
+
+  if (error) throw new Error("Create Wanna Error");
+};
+
+export const updateWanna = async (id: number, data: TaskFormData) => {
+  const user = await currentUser();
+  if (!user) throw new Error("Unauthorized");
+
+  const res = taskFormSchema.parse(data);
+
+  const supabase = await createClient();
+  const { error } = await supabase
+    .from("wannas")
+    .update({ ...res })
+    .eq("id", id);
+
+  if (error) throw new Error("Update Wanna Error");
+};
+
+export const deleteWanna = async (id: number) => {
+  const user = await currentUser();
+  if (!user) throw new Error("Unauthorized");
+
+  const supabase = await createClient();
+  const { error } = await supabase.from("wannas").delete().eq("id", id);
+
+  if (error) throw new Error("Delete Wanna Error");
+};
+
+export const migrateToTodo = async (id: number) => {
+  const user = await currentUser();
+  if (!user) throw new Error("Unauthorized");
+
+  const data = await getWanna(id);
+  if (!data) throw new Error("The Wanna Is Not Found");
+
+  const supabase = await createClient();
+  const { error } = await supabase.from("todos").insert({
+    title: data.title,
+    description: data.description,
+    wannaCreatedAt: data.wannaCreatedAt,
+  });
+
+  if (error) throw new Error("Migrate to Todo Error");
+
+  await deleteWanna(id);
+};
