2deda5bdc5f638d21500609b1898738354eab530
ruriko5 <hyugguurhjyfh174@gmail.com>
2025-10-23 23:28:18 +0900

    feat: dones/[id]/page

--- app/(main)/dones/[id]/page.tsx
+++ app/(main)/dones/[id]/page.tsx
@@ -1,3 +1,22 @@
-export default function DonePage() {
-  return <main>Done Page</main>;
+import { verifyUser } from "@/app/auth/data";
+import { getDone } from "../data";
+import { TaskDetailCard } from "../../components/task/task-detail-card";
+
+export default async function DonePage({ params }: PageProps<"/dones/[id]">) {
+  await verifyUser();
+
+  const doneId = (await params).id;
+  const done = await getDone(Number(doneId));
+
+  if (!done) {
+    return <p className="my-10 text-center">not found</p>;
+  }
+
+  return (
+    <main className="flex flex-1 flex-col gap-4 max-w-2xl mx-auto container">
+      <h2>Done Page</h2>
+
+      <TaskDetailCard task={done} />
+    </main>
+  );
 }
